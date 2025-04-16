16066c8c77ffdb6fd2486a7ece07be78082ccfd2
jnors <jnors@ua.pt>
2025-04-16 13:49:04 +0100

    added url import.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 import path from 'path'
+import { fileURLToPath } from 'url';
+
+// Get the directory name of the current module
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('next').NextConfig} */
